e51daf47a1053676988791ab7ff73388340efbd9
Desli Mane <desli.developer@gmail.com>
2020-04-08 21:42:31 +0200

    put the server and db into their own packages

--- main.go
+++ main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	ac "github.com/desli94/contactbook/adapters/contact"
-	i "github.com/desli94/contactbook/infrastructure"
+	postgres "github.com/desli94/contactbook/infrastructure/postgres"
+	restAPI "github.com/desli94/contactbook/infrastructure/rest"
 	uc "github.com/desli94/contactbook/usecases/contact"
 )
 
 func main() {
 
-	db := i.NewDB()
+	db := postgres.NewDB()
 	contactRepo := ac.NewRepo(db)
 	contactInteractor := uc.NewContactInteractor(contactRepo)
 	contactController := ac.NewContactController(contactInteractor)
 
-	i.CreateServer(contactController)
+	restAPI.CreateServer(contactController)
 
 }
